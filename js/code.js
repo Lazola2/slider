a5f2c1c67820a1332a129a283ffc93e228a320e8
Lazola2 <lazolamakubalo271@gmail.com>
2023-04-21 16:26:59 +0200

    sdf

--- js/code.js
+++ js/code.js
@@ -3,25 +3,44 @@ let btnNext = document.querySelector('.next');
 
 // function to slide forward
 function slideForward(){
+    // swipe all slides
     slides.forEach(slide => {
         // swipe the first element
-        if ( slide.classList.value.includes('first') ) {
+        if ( slide.classList.value.includes('first') && slide.classList.value.includes('unedited')) {
             slide.classList.remove('first'); 
             slide.classList.add('selected'); 
+           
+            slide.classList.remove('unedited'); 
+            slide.classList.add('edited'); 
         }
-        
-        // swipe the selected element
-        if ( slide.classList.value.includes('selected') ) {
+
+        // swipe the first element
+        if ( slide.classList.value.includes('selected') && slide.classList.value.includes('unedited')) {
             slide.classList.remove('selected'); 
             slide.classList.add('last'); 
+            
+            slide.classList.remove('unedited'); 
+            slide.classList.add('edited'); 
         }
-        
-        // swipe the last element
-        if ( slide.classList.value.includes('last') ) {
+         
+        // swipe the first element
+        if ( slide.classList.value.includes('last') && slide.classList.value.includes('unedited')) {
             slide.classList.remove('last'); 
-            slide.classList.add('first'); 
+            slide.classList.add('first');
+
+            slide.classList.remove('unedited'); 
+            slide.classList.add('edited'); 
         }
+    
     })
+
+    // change all slides classes to edited
+    slides.forEach(slide => {
+        if (slide.classList.value.includes('edited')) {
+            slide.classList.remove('edited');
+            slide.classList.add('unedited');
+        }
+    });
 }
 
 btnNext.addEventListener('click', () => {
